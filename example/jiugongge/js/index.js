85bc40bf4e897c32083d3a31bff180cbe78c6cc8
wangchloe <wangchloe@yeah.net>
2018-02-01 17:16:13 +0800

    九宫格转盘上线

--- example/jiugongge/js/index.js
+++ example/jiugongge/js/index.js
@@ -1,5 +1,5 @@
 (function(){
-
+    // 慢 -> 快 -> 慢
     $.squaredLotteryRoll({
         rollWrapSelector: "#J_lottery_container_1",
         prizeIndex: 1,
@@ -11,11 +11,12 @@
     });
 
 
-
+    // 快 -> 慢
     $("#J_btn_start_2").on("click", function(){
         $("#J_lottery_container_2").addClass("mode-2");
 
         $.squaredLotteryRoll({
+            speed: 39,
             rollWrapSelector: "#J_lottery_container_2",
             prizeIndex: 6,
             isDefaultClickFn: false,
